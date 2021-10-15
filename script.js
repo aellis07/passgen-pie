f7b9903c6fa889d8f81ec54e70c00574203fd643
Anthony Ellis <amellis245@gmail.com>
2021-10-14 22:38:17 -0400

    added console to check array showed up w/o error

--- script.js
+++ script.js
@@ -11,81 +11,87 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword() {
-  var lowerChar = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  var upperChar = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChar = [
-    "!",
-    "%",
-    "&",
-    ",",
-    "*",
-    "+",
-    "-",
-    ".",
-    "/",
-    "<",
-    ">",
-    "?",
-    "~",
-  ];
-}
+// GLOBAL VARIABLES
 
-// Add event listener to generate button
-// Carries out the function when the button is clicked
-generateBtn.addEventListener("click", writePassword);
+var lowerChar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+console.log(lowerChar);
+var upperChar = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+console.log(upperChar);
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+console.log(number);
+var specialChar = [
+  "!",
+  "%",
+  "&",
+  ",",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  "<",
+  ">",
+  "?",
+  "~",
+];
+console.log(specialChar);
+
+function generatePassword() {
+  // Add event listener to generate button
+  // Carries out the function when the button is clicked
+  generateBtn.addEventListener("click", writePassword);
+}
